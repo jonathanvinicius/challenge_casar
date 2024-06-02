fab4ab7f173a888bcda894a095fc73ee4bd4396d
jonathanvinicius <47425196+jonathanvinicius@users.noreply.github.com>
2024-06-02 17:23:24 -0300

    feat: create user dto and applying validation rules for user registration

--- nestjs/src/modules/user/dto/create-user.dto.ts
+++ nestjs/src/modules/user/dto/create-user.dto.ts
@@ -1,1 +1,9 @@
-export class CreateUserDto {}
+import { IsNotEmpty, Length } from 'class-validator';
+
+export class CreateUserDto {
+	@IsNotEmpty({ message: 'username is required' })
+	@Length(1, 14, { message: 'username must be between 1 and 14 characters' })
+	username: string;
+
+	createdAt: Date;
+}
