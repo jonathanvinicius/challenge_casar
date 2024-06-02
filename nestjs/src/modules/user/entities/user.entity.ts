4d1a3b84987ebee34b6bf257dbadc4f2dbdfa9a7
jonathanvinicius <47425196+jonathanvinicius@users.noreply.github.com>
2024-06-02 17:28:21 -0300

    feat: create user entity

--- nestjs/src/modules/user/entities/user.entity.ts
+++ nestjs/src/modules/user/entities/user.entity.ts
@@ -1,1 +1,18 @@
-export class User {}
+import { Schema, Prop, SchemaFactory } from '@nestjs/mongoose';
+import * as moment from 'moment';
+
+@Schema({ collection: 'users' })
+export class User {
+	@Prop({ required: true })
+	username: string;
+
+	@Prop({ required: false })
+	createdAt: string;
+}
+
+export const UserSchema = SchemaFactory.createForClass(User);
+
+UserSchema.pre('save', function (next) {
+	this.createdAt = moment().format('DD/MM/YYYY');
+	next();
+});
