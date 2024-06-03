6fa77d0e4b3d49178ca8c79f5776d0c673c20726
jonathanvinicius <47425196+jonathanvinicius@users.noreply.github.com>
2024-06-03 11:45:36 -0300

    refactor: add new method getOne in user repository

--- nestjs/src/modules/user/repository/interfaces/IUser.repository.ts
+++ nestjs/src/modules/user/repository/interfaces/IUser.repository.ts
@@ -1,5 +1,7 @@
 import { CreateUserDto } from '../../dto/create-user.dto';
+import { GetOneUserDto } from '../../dto/get.one.user.dto';
 
 export interface IUserRepository {
 	save(user: CreateUserDto);
+	getOne(param: GetOneUserDto);
 }
--- nestjs/src/modules/user/repository/user.repository.ts
+++ nestjs/src/modules/user/repository/user.repository.ts
@@ -3,13 +3,42 @@ import { User } from '../entities/user.entity';
 import { IUserRepository } from './interfaces/IUser.repository';
 import { Model } from 'mongoose';
 import { InjectModel } from '@nestjs/mongoose';
+import { GetOneUserDto } from '../dto/get.one.user.dto';
+import * as mongoose from 'mongoose';
 
 export class UserRepository implements IUserRepository {
 	constructor(@InjectModel(User.name) private readonly userModel: Model<User>) {}
 
-	save(user: CreateUserDto) {
+	async save(user: CreateUserDto) {
 		const createUser = new this.userModel(user);
 
 		return createUser.save();
+	}
+
+	async getOne(id: GetOneUserDto) {
+		const user = await this.userModel
+			.aggregate([
+				{
+					$match: { _id: new mongoose.Types.ObjectId(id.toString()) },
+				},
+				{
+					$lookup: {
+						from: 'posts',
+						localField: '_id',
+						foreignField: 'author',
+						as: 'posts',
+					},
+				},
+				{
+					$project: {
+						username: 1,
+						createdAt: 1,
+						posts: 1,
+						postsCount: { $size: '$posts' },
+					},
+				},
+			])
+			.exec();
+		return user;
 	}
 }
