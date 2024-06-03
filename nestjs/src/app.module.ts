d472e8f500bf5707c447aeb5128fce6655263a36
jonathanvinicius <47425196+jonathanvinicius@users.noreply.github.com>
2024-06-03 11:43:18 -0300

    refactor: adding new module post

--- nestjs/src/app.module.ts
+++ nestjs/src/app.module.ts
@@ -1,10 +1,13 @@
 import { Module } from '@nestjs/common';
-import { UserService } from './modules/user/user.service';
+import { MongooseModule } from '@nestjs/mongoose';
+import { CreateUserUseCase } from './modules/user/useCase/createUser/create.user.useCase';
 import { UserModule } from './modules/user/user.module';
+import { SeedModule } from './modules/seeds/seeds.module';
+import { PostModule } from './modules/posts/post.module';
 
 @Module({
-  imports: [ModulesModule, UserModule],
-  controllers: [AppController],
-  providers: [AppService, UserService],
+	imports: [UserModule, SeedModule, PostModule, MongooseModule.forRoot('mongodb://localhost:27017/casar')],
+	controllers: [],
+	providers: [CreateUserUseCase],
 })
 export class AppModule {}
