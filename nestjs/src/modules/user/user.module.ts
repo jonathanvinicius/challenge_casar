f9d2871731b0aa495055e474ae4b75e50161f004
jonathanvinicius <47425196+jonathanvinicius@users.noreply.github.com>
2024-06-03 11:46:13 -0300

    refactor: add new useCase getOne user

--- nestjs/src/modules/user/user.module.ts
+++ nestjs/src/modules/user/user.module.ts
@@ -4,12 +4,14 @@ import { UserRepository } from './repository/user.repository';
 import { CreateUserUseCase } from './useCase/createUser/create.user.useCase';
 import { UserSchema } from './entities/user.entity';
 import { MongooseModule } from '@nestjs/mongoose';
+import { GetOneUserUseCase } from './useCase/getOne/get.one.user.useCase';
 
 @Module({
 	imports: [MongooseModule.forFeature([{ name: 'User', schema: UserSchema }])],
 	controllers: [UserController],
 	providers: [
 		CreateUserUseCase,
+		GetOneUserUseCase,
 		{
 			provide: 'IUserRepository',
 			useClass: UserRepository,
