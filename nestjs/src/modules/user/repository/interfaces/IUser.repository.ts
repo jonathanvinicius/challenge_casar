927612b7c22ced4c649283cdda61467aca432ec7
jonathanvinicius <47425196+jonathanvinicius@users.noreply.github.com>
2024-06-04 19:04:12 -0300

    refactor: adding two new methods in interface

--- nestjs/src/modules/user/repository/interfaces/IUser.repository.ts
+++ nestjs/src/modules/user/repository/interfaces/IUser.repository.ts
@@ -1,7 +1,9 @@
 import { CreateUserDto } from '../../dto/create-user.dto';
 import { GetOneUserDto } from '../../dto/get.one.user.dto';
+import { GetOneUserPostsDto } from '../../dto/get.one.user.posts.dto';
 
 export interface IUserRepository {
 	save(user: CreateUserDto);
-	getOne(param: GetOneUserDto);
+	getOneUserAndPosts(params: GetOneUserPostsDto);
+	getOne(params: GetOneUserDto);
 }
