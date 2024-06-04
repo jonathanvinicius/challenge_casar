f3086266b5d7e552a97f7742ff504c63d7af266b
jonathanvinicius <47425196+jonathanvinicius@users.noreply.github.com>
2024-06-04 19:09:32 -0300

    refactor: add new method get in post controller

--- nestjs/src/modules/posts/controller/post.controller.ts
+++ nestjs/src/modules/posts/controller/post.controller.ts
@@ -1,13 +1,22 @@
-import { Controller, Post, Body, Param } from '@nestjs/common';
-import { CreatePostUseCase } from '../useCase/create.post.useCase';
+import { Controller, Post, Body, Param, Query, Get } from '@nestjs/common';
 import { CreatePostDto } from '../dto/create.post.dto';
+import { CreatePostUseCase } from '../useCase/createPost/create.post.useCase';
+import { GetPostUseCase } from '../useCase/getPost/get.post.useCase';
 
 @Controller('post')
 export class PostController {
-	constructor(private readonly createPostUseCase: CreatePostUseCase) {}
+	constructor(
+		private readonly createPostUseCase: CreatePostUseCase,
+		private readonly getPostUseCase: GetPostUseCase,
+	) {}
 
 	@Post('/:author')
-	save(@Body() post: CreatePostDto, @Param('author') author: string) {
+	async save(@Body() post: CreatePostDto, @Param('author') author: string) {
 		return this.createPostUseCase.save({ ...post, author });
+	}
+
+	@Get()
+	async get(@Query('page') page: number, @Query('limit') limit: number, @Query('id') id: string) {
+		return this.getPostUseCase.get({ page, limit, id });
 	}
 }
