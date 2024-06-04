3472bd73271fbf50ec9f37c853c4bce602377952
jonathanvinicius <47425196+jonathanvinicius@users.noreply.github.com>
2024-06-04 19:11:28 -0300

    feat: create new entinty follower

---
+++ nestjs/src/modules/followers/entities/follower.entity.ts
@@ -0,0 +1,24 @@
+import { Schema, Prop, SchemaFactory } from '@nestjs/mongoose';
+import { Document } from 'mongoose';
+import * as mongoose from 'mongoose';
+import { User } from 'src/modules/user/entities/user.entity';
+import * as moment from 'moment';
+
+@Schema({ collection: 'followers', versionKey: false })
+export class Follower extends Document {
+	@Prop({ type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true })
+	userId: User;
+
+	@Prop({ type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true })
+	following: User;
+
+	@Prop({ required: false })
+	createdAt: Date;
+}
+
+export const FollowerSchema = SchemaFactory.createForClass(Follower);
+
+FollowerSchema.pre('save', function (next) {
+	this.createdAt = moment().toDate();
+	next();
+});
