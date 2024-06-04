a8d2e345c923d8b6010a8e5a6e25b7abbdd4f24e
jonathanvinicius <47425196+jonathanvinicius@users.noreply.github.com>
2024-06-04 19:09:05 -0300

    refactor: add new validate for save post

--- nestjs/src/modules/posts/dto/create.post.dto.ts
+++ nestjs/src/modules/posts/dto/create.post.dto.ts
@@ -1,9 +1,27 @@
-import { IsNotEmpty } from 'class-validator';
+import { IsNotEmpty, Length, IsEnum, ValidateIf } from 'class-validator';
+import { TypePost } from '../entities/post.entity';
 
 export class CreatePostDto {
 	@IsNotEmpty({ message: 'content is required' })
+	@Length(1, 200, { message: 'content must be between 1 and 200 characters' })
 	content: string;
-	media: string;
+
 	author: string;
+
+	@IsNotEmpty({ message: 'type is required' })
+	@IsEnum(TypePost, { message: 'type must be a valid enum value' })
+	type: TypePost;
+
+	@ValidateIf(
+		(object) =>
+			(object.type === TypePost.RepostComment && !object.postId) || (object.type === TypePost.Repost && !object.postId),
+	)
+	@IsNotEmpty({ message: 'postId is required for this type of post' })
+	postId?: string;
+
+	@ValidateIf((object) => object.type === 'repostComment')
+	@IsNotEmpty({ message: 'repostComment is required for this type of post' })
+	repostComment?: string;
+
 	createdAt: Date;
 }
