4f6144c0954bdda3aa393a542e77d27912d5f601
jonathanvinicius <47425196+jonathanvinicius@users.noreply.github.com>
2024-06-04 19:08:14 -0300

    refactor: add new enum and type for post entity

--- nestjs/src/modules/posts/entities/post.entity.ts
+++ nestjs/src/modules/posts/entities/post.entity.ts
@@ -2,20 +2,38 @@ import { Schema, Prop, SchemaFactory } from '@nestjs/mongoose';
 import { Document } from 'mongoose';
 import { User } from 'src/modules/user/entities/user.entity';
 import * as mongoose from 'mongoose';
+import * as moment from 'moment';
 
-@Schema({ collection: 'posts' })
+export enum TypePost {
+	Repost = 'repost',
+	Post = 'post',
+	RepostComment = 'repostComment',
+}
+
+@Schema({ collection: 'posts', versionKey: false, timestamps: true })
 export class Post extends Document {
 	@Prop({ required: true })
 	content: string;
 
-	@Prop({ required: false })
-	media: string;
-
 	@Prop({ type: mongoose.Schema.Types.ObjectId, ref: 'User' })
 	author: User;
 
+	@Prop({ required: true })
+	type: TypePost;
+
+	@Prop({ type: mongoose.Schema.Types.ObjectId, ref: 'Post' })
+	postId: Post;
+
 	@Prop({ required: false })
-	createdAt: string;
+	repostComment: string;
+
+	@Prop({ required: false })
+	createdAt: Date;
 }
 
 export const PostSchema = SchemaFactory.createForClass(Post);
+
+PostSchema.pre('save', function (next) {
+	this.createdAt = moment().toDate();
+	next();
+});
