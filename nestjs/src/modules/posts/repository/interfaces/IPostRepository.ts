a658b62302d6e1d1ca887767f98461fbbe66e86c
jonathanvinicius <47425196+jonathanvinicius@users.noreply.github.com>
2024-06-04 19:07:22 -0300

    feat: add new methods in interface post repository

--- nestjs/src/modules/posts/repository/interfaces/IPostRepository.ts
+++ nestjs/src/modules/posts/repository/interfaces/IPostRepository.ts
@@ -1,5 +1,9 @@
 import { CreatePostDto } from '../../dto/create.post.dto';
+import { GetPostByFollowers } from '../../dto/get.post.by.followers';
+import { GetPostDto } from '../../dto/get.post.dto';
 
 export interface IPostRepository {
 	save(post: CreatePostDto);
+	get(params: GetPostDto);
+	getPostByFollowers(params: GetPostByFollowers);
 }
