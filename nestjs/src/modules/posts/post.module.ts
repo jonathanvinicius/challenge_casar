3ae83da175383751bc52a8688fd4a9e4e2c3b82c
jonathanvinicius <47425196+jonathanvinicius@users.noreply.github.com>
2024-06-04 19:07:47 -0300

    refactor: add new dependencies in post module

--- nestjs/src/modules/posts/post.module.ts
+++ nestjs/src/modules/posts/post.module.ts
@@ -1,18 +1,38 @@
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 import { PostRepository } from './repository/post.repository';
-import { CreatePostUseCase } from './useCase/create.post.useCase';
 import { PostSchema } from './entities/post.entity';
 import { PostController } from './controller/post.controller';
+import { CreatePostUseCase } from './useCase/createPost/create.post.useCase';
+import { GetPostUseCase } from './useCase/getPost/get.post.useCase';
+import { FollowerRepository } from '../followers/repository/follower.repository';
+import { FollowerSchema } from '../followers/entities/follower.entity';
+import { UserRepository } from '../user/repository/user.repository';
+import { UserSchema } from '../user/entities/user.entity';
 
 @Module({
-	imports: [MongooseModule.forFeature([{ name: 'Post', schema: PostSchema }])],
+	imports: [
+		MongooseModule.forFeature([
+			{ name: 'Post', schema: PostSchema },
+			{ name: 'Follower', schema: FollowerSchema },
+			{ name: 'User', schema: UserSchema },
+		]),
+	],
 	controllers: [PostController],
 	providers: [
 		CreatePostUseCase,
+		GetPostUseCase,
 		{
 			provide: 'IPostRepository',
 			useClass: PostRepository,
+		},
+		{
+			provide: 'IFollowerRepository',
+			useClass: FollowerRepository,
+		},
+		{
+			provide: 'IUserRepository',
+			useClass: UserRepository,
 		},
 	],
 	exports: [CreatePostUseCase, 'IPostRepository'],
