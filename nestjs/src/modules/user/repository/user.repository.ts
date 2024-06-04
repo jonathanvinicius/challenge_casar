625697510ccf8922102038621b463098fae9cdc6
jonathanvinicius <47425196+jonathanvinicius@users.noreply.github.com>
2024-06-04 19:03:55 -0300

    refactor: add new method getOne and adding join followers

--- nestjs/src/modules/user/repository/user.repository.ts
+++ nestjs/src/modules/user/repository/user.repository.ts
@@ -3,23 +3,29 @@ import { User } from '../entities/user.entity';
 import { IUserRepository } from './interfaces/IUser.repository';
 import { Model } from 'mongoose';
 import { InjectModel } from '@nestjs/mongoose';
-import { GetOneUserDto } from '../dto/get.one.user.dto';
 import * as mongoose from 'mongoose';
+import { GetOneUserPostsDto } from '../dto/get.one.user.posts.dto';
+import { GetOneUserDto } from '../dto/get.one.user.dto';
 
 export class UserRepository implements IUserRepository {
-	constructor(@InjectModel(User.name) private readonly userModel: Model<User>) {}
+	constructor(@InjectModel(User.name) private readonly userRepository: Model<User>) {}
 
 	async save(user: CreateUserDto) {
-		const createUser = new this.userModel(user);
+		const createUser = new this.userRepository(user);
 
 		return createUser.save();
 	}
 
-	async getOne(id: GetOneUserDto) {
-		const user = await this.userModel
+	async getOneUserAndPosts(params: GetOneUserPostsDto) {
+		const limitPage = +params.limit ? params.limit : 5;
+		const currentPage = +params.page ? params.page : 1;
+
+		const skip = limitPage * (currentPage - 1);
+
+		const user = await this.userRepository
 			.aggregate([
 				{
-					$match: { _id: new mongoose.Types.ObjectId(id.toString()) },
+					$match: { _id: new mongoose.Types.ObjectId(params.id.toString()) },
 				},
 				{
 					$lookup: {
@@ -30,15 +36,46 @@ export class UserRepository implements IUserRepository {
 					},
 				},
 				{
+					$lookup: {
+						from: 'followers',
+						localField: '_id',
+						foreignField: 'userId',
+						as: 'following',
+					},
+				},
+				{
+					$lookup: {
+						from: 'followers',
+						localField: '_id',
+						foreignField: 'follower',
+						as: 'followers',
+					},
+				},
+
+				{
+					$skip: Number(skip),
+				},
+				{
+					$limit: Number(limitPage),
+				},
+				{
 					$project: {
 						username: 1,
 						createdAt: 1,
 						posts: 1,
+						following: { $size: '$following' },
+						followers: { $size: '$followers' },
 						postsCount: { $size: '$posts' },
 					},
 				},
 			])
+			.sort({ createdAt: -1 })
 			.exec();
 		return user;
+	}
+
+	async getOne(params: GetOneUserDto) {
+		const id = new mongoose.Types.ObjectId(params.id.toString());
+		return this.userRepository.findById(id);
 	}
 }
