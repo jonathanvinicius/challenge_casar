95f86c1183b88fedbf430d93daaf22c43ecd3ac9
jonathanvinicius <47425196+jonathanvinicius@users.noreply.github.com>
2024-06-05 11:37:04 -0300

    refactor: importing modules necessary

--- nestjs/src/modules/followers/follower.module.ts
+++ nestjs/src/modules/followers/follower.module.ts
@@ -5,9 +5,17 @@ import { FollowersController } from './controller/follower.controller';
 import { FollowerRepository } from './repository/follower.repository';
 import { CreateFollowerUseCase } from './useCase/createFollower/create.follower.useCase';
 import { DeleteFollowerUseCase } from './useCase/deleteFollower/delete.follower.useCase';
+import { ValidateUser } from './util/validateUser';
+import { UserRepository } from '../user/repository/user.repository';
+import { UserSchema } from '../user/entities/user.entity';
 
 @Module({
-	imports: [MongooseModule.forFeature([{ name: 'Follower', schema: FollowerSchema }])],
+	imports: [
+		MongooseModule.forFeature([
+			{ name: 'Follower', schema: FollowerSchema },
+			{ name: 'User', schema: UserSchema },
+		]),
+	],
 	controllers: [FollowersController],
 	providers: [
 		CreateFollowerUseCase,
@@ -16,7 +24,15 @@ import { DeleteFollowerUseCase } from './useCase/deleteFollower/delete.follower.
 			provide: 'IFollowerRepository',
 			useClass: FollowerRepository,
 		},
+		{
+			provide: 'IUserRepository',
+			useClass: UserRepository,
+		},
+		{
+			provide: 'ValidateUser',
+			useClass: ValidateUser,
+		},
 	],
-	exports: [CreateFollowerUseCase, 'IFollowerRepository'],
+	exports: [CreateFollowerUseCase, 'IFollowerRepository', 'IUserRepository', 'ValidateUser'],
 })
 export class FollowerModule {}
