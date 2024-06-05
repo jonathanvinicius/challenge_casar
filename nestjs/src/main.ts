e1f7080a374ad4137f389518f30a8b1f5377e968
jonathanvinicius <47425196+jonathanvinicius@users.noreply.github.com>
2024-06-05 10:22:26 -0300

    refactor: adding swagger in application

--- nestjs/src/main.ts
+++ nestjs/src/main.ts
@@ -1,10 +1,16 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
 	const app = await NestFactory.create(AppModule);
+	const config = new DocumentBuilder().setTitle('Thoughts API').setVersion('1.0').build();
+
+	const document = SwaggerModule.createDocument(app, config);
+	SwaggerModule.setup('docs', app, document);
 	app.useGlobalPipes(new ValidationPipe());
+
 	await app.listen(3000);
 }
 bootstrap();
