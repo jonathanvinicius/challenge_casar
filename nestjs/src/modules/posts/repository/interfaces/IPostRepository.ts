9222c8d4dc53d907e54284a9667b4668e8279f0d
jonathanvinicius <47425196+jonathanvinicius@users.noreply.github.com>
2024-06-05 10:31:54 -0300

    refactor: add new method getPostUser

--- nestjs/src/modules/posts/repository/interfaces/IPostRepository.ts
+++ nestjs/src/modules/posts/repository/interfaces/IPostRepository.ts
@@ -1,9 +1,11 @@
 import { CreatePostDto } from '../../dto/create.post.dto';
 import { GetPostByFollowers } from '../../dto/get.post.by.followers';
 import { GetPostDto } from '../../dto/get.post.dto';
+import { GetPostUserDto } from '../../dto/get.post.user.dto';
 
 export interface IPostRepository {
 	save(post: CreatePostDto);
 	get(params: GetPostDto);
 	getPostByFollowers(params: GetPostByFollowers);
+	getPostUser(params: GetPostUserDto);
 }
