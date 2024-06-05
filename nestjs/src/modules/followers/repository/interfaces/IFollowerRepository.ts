d6c729eb6c9a700bf65c8de955270bc6864d00bd
jonathanvinicius <47425196+jonathanvinicius@users.noreply.github.com>
2024-06-05 11:36:27 -0300

    refactor: changing dto

--- nestjs/src/modules/followers/repository/interfaces/IFollowerRepository.ts
+++ nestjs/src/modules/followers/repository/interfaces/IFollowerRepository.ts
@@ -1,11 +1,10 @@
-import { CreateFollowerDto } from '../../dto/create.follower.dto';
-import { DeleteFollowerDto } from '../../dto/delete.follower.dto';
+import { FollowerDto } from '../../dto/follower.dto';
 import { GetOneResponseDto } from '../../dto/get.one.response.dto';
 import { GetOneFollowerDto } from '../../dto/getOne.follower.dto';
 
 export interface IFollowerRepository {
-	save(follower: CreateFollowerDto);
-	delete(params: DeleteFollowerDto): Promise<void>;
+	save(follower: FollowerDto);
+	delete(params: FollowerDto): Promise<void>;
 	getOne(params: GetOneFollowerDto): Promise<GetOneResponseDto>;
 	get(params: GetOneFollowerDto);
 }
