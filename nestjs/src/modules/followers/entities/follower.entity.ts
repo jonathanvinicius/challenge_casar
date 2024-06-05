0392906689f953bbc0456132acb078814cab630a
jonathanvinicius <47425196+jonathanvinicius@users.noreply.github.com>
2024-06-05 10:23:32 -0300

    refactor: add timezone for create follower

--- nestjs/src/modules/followers/entities/follower.entity.ts
+++ nestjs/src/modules/followers/entities/follower.entity.ts
@@ -2,7 +2,7 @@ import { Schema, Prop, SchemaFactory } from '@nestjs/mongoose';
 import { Document } from 'mongoose';
 import * as mongoose from 'mongoose';
 import { User } from 'src/modules/user/entities/user.entity';
-import * as moment from 'moment';
+import * as moment from 'moment-timezone';
 
 @Schema({ collection: 'followers', versionKey: false })
 export class Follower extends Document {
@@ -19,6 +19,6 @@ export class Follower extends Document {
 export const FollowerSchema = SchemaFactory.createForClass(Follower);
 
 FollowerSchema.pre('save', function (next) {
-	this.createdAt = moment().toDate();
+	this.createdAt = moment().tz('America/Sao_Paulo').toDate();
 	next();
 });
