fd037d0f62eb873a7dbea244e29914bab8ea6b19
jonathanvinicius <47425196+jonathanvinicius@users.noreply.github.com>
2024-06-05 11:36:15 -0300

    refactor: removing business rules for responsible class

--- nestjs/src/modules/followers/useCase/createFollower/create.follower.useCase.ts
+++ nestjs/src/modules/followers/useCase/createFollower/create.follower.useCase.ts
@@ -1,12 +1,17 @@
 import { Inject, Injectable } from '@nestjs/common';
 import { IFollowerRepository } from '../../repository/interfaces/IFollowerRepository';
-import { CreateFollowerDto } from '../../dto/create.follower.dto';
+import { ValidateUser } from '../../util/validateUser';
+import { FollowerDto } from '../../dto/follower.dto';
 
 @Injectable()
 export class CreateFollowerUseCase {
-	constructor(@Inject('IFollowerRepository') private readonly followerRepository: IFollowerRepository) {}
+	constructor(
+		@Inject('IFollowerRepository') private readonly followerRepository: IFollowerRepository,
+		@Inject('ValidateUser') private readonly validateUser: ValidateUser,
+	) {}
 
-	async execute(follower: CreateFollowerDto) {
+	async execute(follower: FollowerDto) {
+		await this.validateUser.validate(follower);
 		return this.followerRepository.save(follower);
 	}
 }
--- nestjs/src/modules/followers/useCase/deleteFollower/delete.follower.useCase.ts
+++ nestjs/src/modules/followers/useCase/deleteFollower/delete.follower.useCase.ts
@@ -1,23 +1,17 @@
 import { DeleteFollowerDto } from '../../dto/delete.follower.dto';
 import { IFollowerRepository } from '../../repository/interfaces/IFollowerRepository';
-import { HttpException, HttpStatus, Inject, Injectable } from '@nestjs/common';
-import * as mongoose from 'mongoose';
+import { Inject, Injectable } from '@nestjs/common';
+import { ValidateUser } from '../../util/validateUser';
 
 @Injectable()
 export class DeleteFollowerUseCase {
-	constructor(@Inject('IFollowerRepository') private readonly followerRepository: IFollowerRepository) {}
+	constructor(
+		@Inject('IFollowerRepository') private readonly followerRepository: IFollowerRepository,
+		@Inject('ValidateUser') private readonly validateUser: ValidateUser,
+	) {}
 
 	async delete(params: DeleteFollowerDto): Promise<void> {
-		if (!mongoose.Types.ObjectId.isValid(params.followerId) || !mongoose.Types.ObjectId.isValid(params.userId)) {
-			throw new HttpException('Invalid user id', HttpStatus.BAD_REQUEST);
-		}
-
-		const follower = await this.followerRepository.getOne({ userId: params.userId, following: params.followerId });
-
-		if (!follower) {
-			throw new HttpException('User or follower not found', HttpStatus.NOT_FOUND);
-		}
-
+		await this.validateUser.validate(params);
 		return this.followerRepository.delete(params);
 	}
 }
